30e5a2066aa5dfc77963ae62a6a71835427d8dd4
mkhuthir <mkhuthir@gmail.com>
2025-07-07 21:42:50 +0400

    u

--- HexapodPlatformIO/include/DynamixelController.h
+++ HexapodPlatformIO/include/DynamixelController.h
@@ -12,10 +12,11 @@ class DynamixelController {
     void  begin(int baudrate, float protocolVersion = DXL_PROTOCOL_VERSION);  // Initialize the controller
     void  initializeServo(uint8_t id, float minAngle, float maxAngle);        // Initialize a servo with its ID and angle limits
 
-    void  setPosition(uint8_t id, float angleDeg);                            // Set servo position in degrees
-    void  setSpeed(uint8_t id, float rpm);                                    // Set servo speed in RPM        
-    float readPosition(uint8_t id);                                           // Read current servo position in degrees  
     bool  isConnected();                                                      // Check if the controller is connected
+    void  setPosition(uint8_t id, float angleDeg);                            // Set servo position in degrees
+    void  setSpeed(uint8_t id, float rpm);                                    // Set servo speed in RPM
+    float readPosition(uint8_t id);                                           // Read current servo position in degrees  
+    
 
   private:
     Dynamixel2Arduino dxl;                // Dynamixel controller instance
