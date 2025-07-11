96a3ead3adcd30416747a388fdf8efdd99250720
Muthanna Alwahash <3192974+mkhuthir@users.noreply.github.com>
2025-07-11 08:39:32 +0400

    Update Leg.cpp

--- SpiderBotVSCode/src/Leg.cpp
+++ SpiderBotVSCode/src/Leg.cpp
@@ -1,20 +1,20 @@
 #include "Leg.h"
 
 Leg::Leg(uint8_t coxaID, uint8_t femurID, uint8_t tibiaID, DynamixelController* controller)
-  : coxa(coxaID), femur(femurID), tibia(tibiaID), dxlCtrl(controller) {}
+  : coxa(coxaID), femur(femurID), tibia(tibiaID), dxl(controller) {}
 
 // Initialize the leg servos
 void Leg::initialize() {
-  dxlCtrl->initializeServo(coxa, COXA_MIN_ANGLE_DEG, COXA_MAX_ANGLE_DEG);       // Initialize coxa servo with limits
-  dxlCtrl->initializeServo(femur, FEMUR_MIN_ANGLE_DEG, FEMUR_MAX_ANGLE_DEG);    // Initialize femur servo with limits
-  dxlCtrl->initializeServo(tibia, TIBIA_MIN_ANGLE_DEG, TIBIA_MAX_ANGLE_DEG);    // Initialize tibia servo with limits
+  dxl->initializeServo(coxa, COXA_MIN_ANGLE_DEG, COXA_MAX_ANGLE_DEG);       // Initialize coxa servo with limits
+  dxl->initializeServo(femur, FEMUR_MIN_ANGLE_DEG, FEMUR_MAX_ANGLE_DEG);    // Initialize femur servo with limits
+  dxl->initializeServo(tibia, TIBIA_MIN_ANGLE_DEG, TIBIA_MAX_ANGLE_DEG);    // Initialize tibia servo with limits
 }
 
 // Set angles for the leg joints
 void Leg::setJointAngles(float coxaAngle, float femurAngle, float tibiaAngle) {
-  dxlCtrl->setGoalPosition(coxa, coxaAngle);      // Set coxa angle
-  dxlCtrl->setGoalPosition(femur, femurAngle);    // Set femur angle
-  dxlCtrl->setGoalPosition(tibia, tibiaAngle);    // Set tibia angle
+  dxl->setGoalPosition(coxa, coxaAngle);      // Set coxa angle
+  dxl->setGoalPosition(femur, femurAngle);    // Set femur angle
+  dxl->setGoalPosition(tibia, tibiaAngle);    // Set tibia angle
 }
 
     float getCoxaAngle(){};                        // Get current coxa angle
