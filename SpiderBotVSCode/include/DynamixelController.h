d12a438e35dce1d2598363a4e43c51a4f96c80f0
Muthanna Alwahash <mkhuthir@gmail.com>
2025-07-11 07:15:57 +0400

    added bulkaction

--- SpiderBotVSCode/include/DynamixelController.h
+++ SpiderBotVSCode/include/DynamixelController.h
@@ -2,6 +2,7 @@
 #define DYNAMIXEL_CONTROLLER_H
 
 #include <DynamixelWorkbench.h>
+#include <vector>
 
 class DynamixelController
 {
@@ -12,6 +13,7 @@ class DynamixelController
 
   public:
     DynamixelController(const char* device, uint32_t baud);
+
     bool begin();
     bool ping(uint8_t id);
     bool torqueOn(uint8_t id);
@@ -19,6 +21,12 @@ class DynamixelController
     bool setGoalPosition(uint8_t id, uint32_t position);
     bool setGoalVelocity(uint8_t id, uint32_t velocity);
     bool readPresentPosition(uint8_t id, uint32_t &position);
+
+    // NEW
+    std::vector<uint8_t> scan(uint8_t start_id = 1, uint8_t end_id = 253);
+    bool syncWritePosition(const std::vector<uint8_t>& ids, const std::vector<uint32_t>& positions);
+    bool bulkReadPositions(const std::vector<uint8_t>& ids, std::vector<uint32_t>& positions);
 };
 
 #endif
+
